c82a7d0bcd2b9633e2ac0ba141ffc8f8323a6b3d
Sazzad <sazzad47.ju@gmail.com>
2022-10-17 08:53:00 -0700

    make it better

--- pages/contact.js
+++ pages/contact.js
@@ -1,30 +1,73 @@
+// import React from 'react'
+// import Breadcrumb from '../components/Breadcrumb'
+
+// import Form from '../components/contact/Form'
+// import Info from '../components/contact/Info'
+// import { getData } from '../utils/fetchData'
+
+
+
+// const Contact = ({data}) => {
+ 
+//   return (
+//  <> 
+//    <Breadcrumb title="Contact"/>
+   
+//     <div className="">
+//       {/* <Info data={data} /> */}
+//       <Form />
+//     </div>
+//   </>
+//   )
+// }
+
+// export async function getServerSideProps() {
+  
+//   const res = await getData(
+//     `contact?index=1`
+//   )
+  
+//   return {
+//     props: {
+//         data: res.data,
+//     }, 
+//   }
+// }
+
+// export default Contact
+
+import { Container, Grid } from '@mui/material'
 import React from 'react'
 import Breadcrumb from '../components/Breadcrumb'
-
-import Form from '../components/contact/Form'
-import Info from '../components/contact/Info'
 import { getData } from '../utils/fetchData'
+import Parser from 'html-react-parser';
 
-
-
-const Contact = ({data}) => {
- 
-  return (
- <> 
-   <Breadcrumb title="Contact"/>
+const Services = ({data}) => {
    
-    <div className="">
-      {/* <Info data={data} /> */}
-      <Form />
-    </div>
-  </>
-  )
+    
+
+    return (
+        <React.Fragment>
+           <Breadcrumb title="Services"/>
+           <Container sx={{minHeight:'100vh'}}>
+            <Grid>
+            {Parser(data.title)}
+            </Grid>
+            <Grid>
+            {Parser(data.shortDescription)}
+            </Grid>
+            <Grid>
+            {Parser(data.body)}
+            </Grid>
+           </Container>
+        </React.Fragment>
+    )
 }
 
 export async function getServerSideProps() {
   
   const res = await getData(
-    `help?index=1`
+    `services?index=1`
   )
   
   return {
@@ -34,4 +77,4 @@ export async function getServerSideProps() {
   }
 }
 
-export default Contact
+export default Services
