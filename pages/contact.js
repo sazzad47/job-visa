e4418a0241ac433f9e25a8c91e6808cead53f303
Sazzad <sazzad47.ju@gmail.com>
2022-10-18 07:03:37 -0700

    make it better

--- pages/contact.js
+++ pages/contact.js
@@ -38,31 +38,20 @@
 
 import React from 'react'
 import Breadcrumb from '../components/Breadcrumb'
-import Notices from '../components/notices/Notices'
-import { getData } from '../utils/fetchData'
-
-import { Icon, InlineIcon } from '@iconify/react'
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
-import EmailIcon from '@mui/icons-material/Email';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Parser from 'html-react-parser';
+import { getData } from '../utils/fetchData'
 
 
 const Notice = ({data}) => {
     return (
         <React.Fragment>
            <Breadcrumb title="Contact"/>
-           <section className="info">
-    <h2 className="info-h2">Contact information</h2>
+          
 
-    {/* <div className="d-flex flex-column">
-      <div className='d-flex mb-2'><LocalPhoneIcon sx={{mr:2}} /> {Parser(data.phone)}</div>
-      <div className='d-flex my-2'><EmailIcon sx={{mr:2}} /> {Parser(data.email)}</div>
-      <div className='d-flex'><LocationOnIcon sx={{mr:2}} /> {Parser(data.address)}</div>
-    </div> */}
+           <div>{Parser(data.email)} </div>
 
     
-  </section>
+ 
             
         </React.Fragment>
     )
@@ -71,7 +60,7 @@ const Notice = ({data}) => {
 export async function getServerSideProps() {
   
     const res = await getData('contact?index=1')
-    console.log('noticeddd', res.data)
+    
     return {
       props: {
           data: res.data,
