a90e4977e9f5c0cd8adc8a834f48827b1ac2f27f
Sazzad <sazzad47.ju@gmail.com>
2022-10-18 11:28:41 -0700

    make it better

--- pages/contact.js
+++ pages/contact.js
@@ -13,7 +13,7 @@
 //  <> 
 //    <Breadcrumb title="Contact"/>
    
-//     <div className="">
+//     <div className="contact-">
 //       {/* <Info data={data} /> */}
 //       <Form />
 //     </div>
@@ -40,18 +40,57 @@ import React from 'react'
 import Breadcrumb from '../components/Breadcrumb'
 import Parser from 'html-react-parser';
 import { getData } from '../utils/fetchData'
-
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const Notice = ({data}) => {
     return (
         <React.Fragment>
            <Breadcrumb title="Contact"/>
-          
-
-           <div>{Parser(data.email)} </div>
-
-    
- 
+          <div className='contact-page-container'>
+            
+           <div className="contact-page">
+    <div className="contact-content">
+      <div className="contact-left-side">
+        <div className="contact-address details">
+          <i className="fas fa-map-marker-alt"></i>
+          <div className="contact-topic">Address</div>
+          <div className="contact-text-one">Surkhet, NP12</div>
+          <div className="contact-text-two">Birendranagar 06</div>
+        </div>
+        <div className="contact-phone details">
+          <i className="fas fa-phone-alt"></i>
+          <div className="contact-topic">Phone</div>
+          <div className="contact-text-one">+0098 9893 5647</div>
+          <div className="contact-text-two">+0096 3434 5678</div>
+        </div>
+        <div className="contact-email details">
+          <i className="fas fa-envelope"></i>
+          <div className="contact-topic">Email</div>
+          <div className="contact-text-one">codinglab@gmail.com</div>
+          <div className="contact-text-two">info.codinglab@gmail.com</div>
+        </div>
+      </div>
+      <div className="contact-right-side">
+        <div className="contact-topic-text">Send us a message</div>
+        <p>If you have any work from me or any types of quries related to my tutorial, you can send me message from here. It's my pleasure to help you.</p>
+      <form action="#">
+        <div className="contact-input-box">
+          <input type="text" placeholder="Enter your name" />
+        </div>
+        <div className="contact-input-box">
+          <input type="text" placeholder="Enter your email" />
+        </div>
+        <div className="contact-input-box message-box">
+          <textarea type="text" placeholder="Enter your message" />
+        </div>
+        <div className="contact-button">
+          <input type="button" value="Send Now" />
+        </div>
+      </form>
+    </div>
+    </div>
+  </div>
+          </div>
             
         </React.Fragment>
     )
